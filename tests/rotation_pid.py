b0c054a8df9a4e8080335fcc3b50f78d58ab2031
omrijsharon <omri_s@yahoo.com>
2023-03-10 18:50:50 +0200

    rotation env working

--- tests/rotation_pid.py
+++ tests/rotation_pid.py
@@ -1,8 +1,10 @@
 import numpy as np
 from utils.components import PID
 import gym
+from time import sleep
 from utils.helper_functions import rotation_matrix_from_euler_angles
 from utils.kinematics import rotate_body_by_rates
+from utils.render3d import plot_3d_rotation_matrix, init_3d_axis, show_plot
 
 class Rotate(gym.Env):
     def __init__(self, dt=1e-2, max_rates=1000, threshold=1e-3):
@@ -16,6 +18,7 @@ class Rotate(gym.Env):
         self.dt = dt
         self.max_rates = max_rates
         self.threshold = threshold
+        self.ax, self.fig = init_3d_axis()
 
     def convert_action_to_rates(self, action):
         return action * self.max_rates
@@ -41,7 +44,10 @@ class Rotate(gym.Env):
 
 
     def render(self, mode='human'):
-        pass
+        self.ax.clear()
+        plot_3d_rotation_matrix(self.ax, self.current_state, np.zeros((3,)), scale=1.0)
+        plot_3d_rotation_matrix(self.ax, self.goal_state, np.zeros((3,)), scale=1.0)
+        show_plot(self.ax, self.fig, middle=None, edge=1.0, title=None, xlabel=None, ylabel=None, zlabel=None, equal=True, grid=True, legend=True)
 
     def close(self):
         pass
@@ -54,3 +60,12 @@ class Rotate(gym.Env):
 
     def __call__(self, x):
         return self.step(x)
+
+
+if __name__ == '__main__':
+    env = Rotate()
+    env.reset()
+    for i in range(100):
+        env.step(env.action_space.sample())
+        env.render()
+        sleep(0.1)
